75a83a0446b4bfd0606453d325ffe28c2c62eca9
IKZmicroPL <robert.kernke@ikz-berlin.de>
2025-10-02 16:43:57 +0200

    Update script_preparation.py

--- MicroPL/Application/script_preparation.py
+++ MicroPL/Application/script_preparation.py
@@ -1,41 +1,95 @@
 import numpy as np
 
-def multi_current_mapping(currents,boundary_points_stage,spatial=True,spectral=False):
+def multi_current_mapping(currents_mA,boundary_points_stage,spatial=True,spectral=False,max_voltage_V=30):
+    # requires electric output already on, stays on last current after execution
     lines=[]
     filter_wheel_open_position="1"
     filter_wheel_closed_position="2"
     lines.append("filter : "+filter_wheel_open_position)
     if not spectral:
-        lines.append("spectral_shutter_mode : closed")
+        lines.append("spectral_shutter_mode : normal")
     else:
         lines.append("spectral_shutter_mode : open")
 
-    for i in range(len(currents)):
+    for i in range(len(currents_mA)):
+        lines.append("current_mA : "+str(currents_mA[i]))
+        if i==0:
+            lines.append("voltage_V : "+str(max_voltage_V))
         line="stage_mapping : "
         line+= "spectral_bool : "+str(spectral)+" , "
         line+= "spatial_bool : "+str(spatial)+" , "
+        line+= "x_min_mm : "+str(boundary_points_stage[0])+" , "
+        line+= "x_max_mm : "+str(boundary_points_stage[1])+" , "
+        line+= "x_num_int : "+str(boundary_points_stage[2])+" , "
+        line+= "y_min_mm : "+str(boundary_points_stage[3])+" , "
+        line+= "y_max_mm : "+str(boundary_points_stage[4])+" , "
+        line+= "y_num_int : "+str(boundary_points_stage[5])   
+
+        lines.append(line) 
+
+
+
+    if spectral:
+        lines.append("spectral_shutter_mode : normal")
+    return lines
+
+
+def multi_step_IV(steps_mA,boundary_points_mA,spatial=True,spectral=True):
+    lines=[]
+    filter_wheel_open_position="1"
+    filter_wheel_closed_position="2"
+    lines.append("filter : "+filter_wheel_open_position)
+    if not spectral:
+        lines.append("spectral_shutter_mode : normal")
+    else:
+        lines.append("spectral_shutter_mode : open")
+
+    for i in range(len(steps_mA)):
+        line="measure_iv_curve_set_currents : "
+        line+= "spectral_bool : "+str(spectral)+" , "
+        line+= "spatial_bool : "+str(spatial)+" , "
+        bp=boundary_points_mA[i]
+        line+="step_current_mA : "+str(steps_mA[i])+" , "
+        line+="start_current_mA : "+str(bp[0])+" , "
+        line+="end_current_mA : "+str(bp[1])+" , "
         
-    
+        lines.append(line) 
 
-def multi_step_IV(steps,boundary_points_mA,spatial=True,spectral=True):
+
+    if spectral:
+        lines.append("spectral_shutter_mode : normal")
+    return lines
+
+def acq_pause_acq_sequence(timestep_s,timerange_s,spatial=True,spectral=True):
+    #timerange_s should be a multiple of timestep_s
     lines=[]
     filter_wheel_open_position="1"
     filter_wheel_closed_position="2"
     lines.append("filter : "+filter_wheel_open_position)
     if not spectral:
-        lines.append("spectral_shutter_mode : closed")
+        lines.append("spectral_shutter_mode : normal")
     else:
         lines.append("spectral_shutter_mode : open")
 
+    repetitions=int(timerange_s/timestep_s)
 
+    if spatial:
+        lines.append("spatial_acquire")
+    if spectral:
+        lines.append("spectral_acquire")
+    lines.append("filter : "+filter_wheel_closed_position)
 
-def acq_pause_acq_sequence(timestep,spatial=True,spectral=True):
-    lines=[]
-    filter_wheel_open_position="1"
-    filter_wheel_closed_position="2"
-    lines.append("filter : "+filter_wheel_open_position)
-    if not spectral:
-        lines.append("spectral_shutter_mode : closed")
-    else:
-        lines.append("spectral_shutter_mode : open")
+    for i in range(repetitions):
+        lines.append("sleep_s : "+str(timestep_s))
+        lines.append("filter : "+filter_wheel_open_position)
 
+        if spatial:
+            lines.append("spatial_acquire")
+        if spectral:
+            lines.append("spectral_acquire")
+        lines.append("filter : "+filter_wheel_closed_position)
+
+    if spectral:
+        lines.append("spectral_shutter_mode : normal")
+
+    return lines
